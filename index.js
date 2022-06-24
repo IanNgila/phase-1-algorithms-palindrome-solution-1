f065f1dcc4a79adff5d74d9c4ec994f9abf66d3b
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-24 09:39:41 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
 /* 
@@ -10,6 +16,7 @@ function isPalindrome(word) {
   Add written explanation of your solution here
 */
 
+
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -18,8 +25,29 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
+module.exports = isPalindrome;
 module.exports = isPalindrome;
